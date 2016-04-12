69129aa527987c80a5560a9afa6747401a2070cf
Nishant9 <nishant.gupta291995@gmail.com>
2016-04-13 03:18:30 +0530

    WIP

--- project/test/test.scala
+++ project/test/test.scala
@@ -1,7 +1,9 @@
 object Test {
+       var iter_count : Int = 0; 
        def println(a: Int) =  {
            a = a + 3;
            if( a == 10 ) {
+               iter_count = iter_count + 1;
                println(a);
                return;
            }
