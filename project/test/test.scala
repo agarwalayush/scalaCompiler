0e3e9fa7fe8c220ce555fb3a2ff1ec77f7cf45b7
Nishant9 <nishant.gupta291995@gmail.com>
2016-04-12 20:36:38 +0530

    WIP : local variables.

--- project/test/test.scala
+++ project/test/test.scala
@@ -1,13 +1,13 @@
 object Test {
-       
+/*       
        def println(a : Int,b: Int) =  {
            return;
        }
-       
+*/       
        val a = 3, b = 5;
 //       println(a,b);
        a = b + 3;
-       return;
+//       return;
 
 
 }
